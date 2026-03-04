06f601da13e60ed0538fd0329d695387cc26c6ef
PranavDingore <chmcs2026.pranav.dingore@gmail.com>
2026-03-04 11:15:24 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/Homepage";
 import CreateItemPage from "./pages/CreateItemPage";
 import ItemDetailPage from "./pages/ItemDetailPage";
 import { Toaster } from "react-hot-toast";
