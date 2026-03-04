22193a8f9a16648646142a1cca1a1c0d59ff1ca8
PranavDingore <chmcs2026.pranav.dingore@gmail.com>
2026-03-04 11:17:40 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: 'https://lost-and-found-backend-zf98.onrender.com/'
 })
 export default api
